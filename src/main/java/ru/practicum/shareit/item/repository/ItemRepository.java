40201594c07c264c578f41910f4606456ddb3eb7
Aleksandr Borisov <aleksandr-zm-borisov@mail.ru>
2023-10-04 19:45:57 +0800

    Чекстайл 1. Реализация бронирования предмета, добавление в проект базы данных, spring data, jpa

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
     List<Item> findAllByOwnerId(Integer id);
+
     @Query("select i from Item i where (lower(i.name) like lower(concat('%', ?1, '%')) " +
             "or lower(i.description) like lower(concat('%', ?1, '%'))) and is_available = true")
     List<Item> search(String text);
